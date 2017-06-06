b37c7a713ee34eee9873f56049ad6a3dfcb078aa
Dragonhaze <duncanporter@hotmail.es>
2017-06-06 09:56:17 +0200

    Modfied main and added showmenu() and other metods to ContactApp

--- src/duncan/Main.java
+++ src/duncan/Main.java
@@ -1,24 +1,14 @@
 package duncan;
 
-import duncan.model.Contact;
+import duncan.controller.ContactApp;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Contact manolo = new Contact(
-                "Manolo",
-                "Sanchez",
-                100000,
-                2002304,
-                2343240,
-                "Calle calatrava nº1");
+        ContactApp contactApp = new ContactApp();
 
-        Contact luis = new Contact(
-                "Luis",
-                1039478);
-
-        System.out.println(manolo.toString());
-        System.out.println(luis.toString());
+        contactApp.start();
     }
 }
--- src/duncan/controller/ContactApp.java
+++ src/duncan/controller/ContactApp.java
@@ -3,6 +3,8 @@ package duncan.controller;
 import duncan.model.Contact;
 import duncan.model.ContactList;
 
+import java.util.Scanner;
+
 /**
  * Created by dunca on 06/06/2017.
  */
@@ -14,6 +16,70 @@ public class ContactApp {
         contactList = new ContactList();
     }
 
+    public void start() {
+        int option;
+
+        while ((option = showMenu()) != 0) {
+            switch (option) {
+                case 1:
+                    contactList.addContact(askContactInfo());
+                    break;
+                case 2:
+                    contactList.removeContact();
+                    break;
+                case 3:
+                    ;
+                    break;
+                case 4:
+                    ;
+                    break;
+                case 5:
+                    ;
+                    break;
+            }
+        }
+    }
+    private Contact askContactInfo() {
+        Scanner scanner = new Scanner(System.in);
+        String name, surname, address;
+        int mobile, home, work;
+
+        do {
+            System.out.println("Código de vuelo: ");
+            name = scanner.next().trim().replaceAll("\\s+", " ");
+        } while (name.equals(""));
+
+        scanner.nextLine();
+
+        do {
+            System.out.println("Compañía Aérea: ");
+            surname = scanner.nextLine().trim().replaceAll("\\s+", " ");
+        } while (surname.equals(""));
+
+        do {
+            System.out.println("Velocidad de crucero: ");
+            mobile = scanner.nextInt();
+        } while (mobile < 0);
+
+        do {
+            System.out.println("Distancia a pista: ");
+            home = scanner.nextInt();
+        } while (home < 0);
+
+        do {
+            System.out.println("Distancia a pista: ");
+            work = scanner.nextInt();
+        } while (work < 0);
+
+        do {
+            System.out.println("Código de vuelo: ");
+            address = scanner.next().trim().replaceAll("\\s+", " ");
+        } while (address.equals(""));
+
+        return new Contact(name, surname, mobile, home, work, address);
+
+    }
+
     public void run(){
         contactList.addContact(new Contact(
                 "Manolo",
@@ -22,9 +88,24 @@ public class ContactApp {
                 2002304,
                 2343240,
                 "Calle calatrava nº1"));
+    }
 
-        contactList.addContact( new Contact(
-                "Luis",
-                1039478));
+    private int showMenu(){
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        System.out.println("****************************");
+        System.out.println("* 1 - Add contact          *");
+        System.out.println("* 2 - Remove contact       *");
+        System.out.println("* 3 - List contacts        *");
+        System.out.println("* 4 - Search contact       *");
+        System.out.println("* 5 - Save contacts        *");
+        System.out.println("* 0 - Exit                 *");
+        System.out.println("****************************");
+        System.out.println("Opción: ");
+
+        option = scanner.nextInt();
+
+        return option;
     }
 }
